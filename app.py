60f5eaef236be0e5735d9d62605840a50bc7de97
zeeshanzaidi92 <90977001+zeeshanzaidi92@users.noreply.github.com>
2021-10-17 12:33:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask, request, url_for, redirect, render_template, jsonify
-import numpy as np
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.classification import *
 import pandas as pd
 import pickle
-from pycaret.classification import *
+import numpy as np
 
 app = Flask(__name__)
 
 model = load_model('Final-ML1000-P1-Heart-20211015')
 cols = ['Age', 'Gender', 'Chest_Pain', 'Blood_Pressure', 'Cholesterol', 'Blood_Sugar', 'Rest_ECG', 'Max_Heart_Rate','Exercise_Angina', 'ST_Depression', 'ST_Slope', 'Marked_Vessels', 'Thallium']
-   
+ 
+
 @app.route('/')
 def home():
-        return render_template('home.html')
-   
-@app.route('predict', methods = ['POST'])
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
 def predict():
-        int_features = [x for x in request.form.values()]
-        final = np.array(int_features)
-        data_unseen = pd.DataFrame([final], columns = cols)
-        prediction = predict_model(model, data = data_unseen, round = 0)
-        prediction = int(prediction.Label[0])
-        return render_template('home.html', pred = 'Heart Disease {}'.format(prediction))
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred='Heart Disease {}'.format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
