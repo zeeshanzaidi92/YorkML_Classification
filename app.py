e0732dbbdf1a422c93070b2cb93c7c996fd2b8e8
zeeshanzaidi92 <90977001+zeeshanzaidi92@users.noreply.github.com>
2021-10-16 23:24:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, url_for, redirect, render_template, jsonify
-pip install pycaret
 import numpy as np
 import pandas as pd
 import pickle
@@ -22,3 +21,14 @@ def predict():
         prediction = predict_model(model, data = data_unseen, round = 0)
         prediction = int(prediction.Label[0])
         return render_template('home.html', pred = 'Heart Disease {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
